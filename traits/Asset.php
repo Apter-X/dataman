0cafba31550b17b927c510f26525465da14e71b6
Iliass Raihani <i.raihani@outlook.com>
2021-05-13 21:22:26 +0000

    Update Asset.php

--- traits/Asset.php
+++ traits/Asset.php
@@ -18,6 +18,8 @@ trait Asset
                 .months ul li a{  float:left;  margin:-1px;  padding:0px 15px 0px 0px;  color:#888888;  text-decoration:none;  font-size:30px;  font-weight:bold;  text-transform:uppercase;}
                 .months ul li a:hover, .months ul li a.active{  color:#D90000;}
                 #calendar{  border-collapse:collapse;}
+                #calendar a {  color:#8C8C8C; text-decoration:none;}
+                #calendar a:hover{  color:#D90000; }
                 #calendar td{  border:1px solid #A3A3A3;  width:80px;  height:70px;}
                 #calendar td.today{  border:2px solid #D90000;  width:80px;  height:70px;}
                 #calendar td.padding{  border:none;}
@@ -37,7 +39,49 @@ trait Asset
                 #datatable tr:hover {background-color: #ddd;}
                 #datatable th {  padding-top: 12px;  padding-bottom: 12px;  text-align: left;  background-color: #D90000;  color: white;}
                 .clear{  clear:both;}
+                .depop-alert{  animation-name: go-top;  animation-duration: 9s;  animation-iteration-count: 1;  visibility: hidden;}@keyframes go-top {0% { top:50px;}20% { top:50px; visibility: visible; }100% { top:-50px; visibility: visible; }}
+                .animate-top{position:relative;animation:animatetop 0.4s}@keyframes animatetop{from{top:-300px;opacity:0} to{top:0;opacity:1}}
+                .animate-left{position:relative;animation:animateleft 0.4s}@keyframes animateleft{from{left:-300px;opacity:0} to{left:0;opacity:1}}
+                .animate-right{position:relative;animation:animateright 0.4s}@keyframes animateright{from{right:-300px;opacity:0} to{right:0;opacity:1}}
+                .animate-bottom{position:relative;animation:animatebottom 0.4s}@keyframes animatebottom{from{bottom:-300px;opacity:0} to{bottom:0;opacity:1}}
             </style>
+        <?php
+    }
+    
+    function dropCalendarJQuery($jqueryPath = null)
+    {
+        if(is_null($jqueryPath))
+        {
+        ?>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous"></script>
+        <?php
+        } else {
+        ?>
+            <script type="text/javascript" src=<?= $jqueryPath ?>></script>
+        <?php
+        }
+        ?>
+            <script type="text/javascript"> 
+                jQuery(function($){
+                    var d = new Date();
+                    var m = d.getMonth() + 1;
+                    $('.month').hide();
+                    $('#month'+m).show();
+                    $('.months a#linkMonth'+m).addClass('active');
+                    var current = m;
+                    $('.months a').click(function(){
+                        var month = $(this).attr('id').replace('linkMonth','');
+                        if(month != current){
+                            $('#month'+current).slideUp();
+                            $('#month'+month).slideDown();
+                            $('.months a').removeClass('active'); 
+                            $('.months a#linkMonth'+month).addClass('active'); 
+                            current = month;
+                        }
+                        return false; 
+                    });
+                });
+            </script> 
         <?php
     }
 }
