70fcf5ad9838b21cd31e06af9e911391eb6903f5
Iliass Raihani <i.raihani@outlook.com>
2021-05-13 21:22:17 +0000

    Update View.php

--- traits/View.php
+++ traits/View.php
@@ -90,62 +90,64 @@ trait View
     /**
     * Display Calendar
     */
-    public function displayCalendar($dates, $date, $year)
+    public function displayCalendar($year, $dates, $date, $events, $route = null)
     {
         ?>
             <div class="periods">
-            <div class="year"><?php echo $year; ?></div>
-            <div class="months">
-                <ul>
-                    <?php foreach ($date->months as $id=>$m): ?>
-                        <li><a href="#" id="linkMonth<?php echo $id+1; ?>"><?php echo substr($m,0,3); ?></a></li>
-                    <?php endforeach; ?>
-                </ul>
-            </div>
-            <div class="clear"></div> 
-            <?php foreach ($dates as $m=>$days) : ?>
-            <div class="month relative" id="month<?php echo $m ?>">
-                <table id="calendar">
-                    <thead>
-                        <tr>
-                        <?php foreach ($date->days as $d): ?>
-                            <th><?php echo substr($d,0,3); ?></th>
+                <div class="year"><?php echo $year; ?></div>
+                <div class="months">
+                    <ul>
+                        <?php foreach ($date->months as $id=>$m): ?>
+                            <li><a href="#" id="linkMonth<?php echo $id+1; ?>"><?php echo substr($m,0,3); ?></a></li>
                         <?php endforeach; ?>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <?php $end = end($days); foreach ($days as $d=>$w): ?>
-                                <?php $time = strtotime("$year-$m-$d"); ?>
-                                <?php if ($d == 1 and $w != 1): ?>
-                                    <td colspan="<?php echo $w-1; ?>" class="padding"></td>
-                                <?php endif; ?>
-                                <td <?php if($time == strtotime(date('Y-m-d'))): ?>class="today"<?php endif; ?>>
-                                    <div class="relative">
-                                        <div class="day"><?php echo $d; ?></div>
-                                    </div>
-                                    <div class="daytitle">
-                                        <?php echo $date->days[$w-1]; ?> <?php echo $d ?> <?php echo $date->months[$m-1]; ?>
-                                    </div>
-                                    <ul class="events">
-                                        <?php if(isset($events[$time])): foreach($events[$time] as $e): ?>
-                                            <li><?php echo $e; ?></li>
-                                        <?php endforeach; endif; ?>
-                                    </ul>
-                                </td>
-                                <?php if ($w == 7): ?>
-                                    </tr><tr>
-                                <?php endif; ?>
+                    </ul>
+                </div>
+                <div class="clear"></div> 
+                <?php foreach ($dates as $m=>$days) : ?>
+                <div class="month relative" id="month<?php echo $m ?>">
+                    <table id="calendar">
+                        <thead>
+                            <tr>
+                            <?php foreach ($date->days as $d): ?>
+                                <th><?php echo substr($d,0,3); ?></th>
                             <?php endforeach; ?>
-                            <?php if ($end != 7): ?>
-                                <td colspan="<?php echo 7-$end; ?>" class="padding"></td>
-                            <?php endif; ?>
-                        </tr>
-                    </tbody>
-                </table>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <?php $end = end($days); foreach ($days as $d=>$w): ?>
+                                    <?php $time = strtotime("$year-$m-$d"); ?>
+                                    <?php if ($d == 1 and $w != 1): ?>
+                                        <td colspan="<?php echo $w-1; ?>" class="padding"></td>
+                                    <?php endif; ?>
+                                    <td <?php if($time == strtotime(date('Y-m-d'))): ?>class="today"<?php endif; ?>>
+                                        
+                                            <div class="relative">
+                                            <div class="day"><a href=<?= "$route?date=$year-$m-$d"; ?>><?php echo $d; ?></a></div>
+                                            </div>
+                                            <div class="daytitle">
+                                                <?php echo $date->days[$w-1]; ?> <?php echo $d ?> <?php echo $date->months[$m-1]; ?>
+                                            </div>
+                                        
+                                        <ul class="events">
+                                            <?php if(isset($events[$time])): foreach($events[$time] as $e): ?>
+                                                <li><?php echo $e; ?></li>
+                                            <?php endforeach; endif; ?>
+                                        </ul>
+                                    </td>
+                                    <?php if ($w == 7): ?>
+                                        </tr><tr>
+                                    <?php endif; ?>
+                                <?php endforeach; ?>
+                                <?php if ($end != 7): ?>
+                                    <td colspan="<?php echo 7-$end; ?>" class="padding"></td>
+                                <?php endif; ?>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            <?php endforeach; ?>
             </div>
-        <?php endforeach; ?>
-        </div>
         <?php
     }
 
@@ -157,11 +159,11 @@ trait View
         $police = POLICE_PRIMARY;
 
         $message = <<<EOT
-            <div style="
+            <div class="depop-alert" style="
                 font-family: $police;
                 border: 1px solid black; 
-                position: relative;
-                width: 100%;
+                position: absolute;
+                width: 97%;
                 background-color: $color
             ">
                 <p style="
