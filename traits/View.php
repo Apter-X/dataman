26dc7415789969452c42f19e6a169f4ff3f70a80
Iliass Raihani <i.raihani@outlook.com>
2021-05-13 14:59:36 +0000

    add display calendar methode

--- traits/View.php
+++ traits/View.php
@@ -20,7 +20,7 @@ trait View
     /**
     * Display table
     */
-    public function displayTable($arr, $keyId, $route = NULL)
+    public function displayTable($arr, $keyId = 'id', $route = NULL)
     {
         $i = 0;
         $count = count($arr);
@@ -56,9 +56,9 @@ trait View
     }
 
     /**
-    * Display table
+    * Display row
     */
-    public function displayRow($arr, $keyId, $route = NULL)
+    public function displayRow($arr, $keyId = 'id', $route = NULL)
     {
         $police = POLICE_PRIMARY;
 
@@ -91,6 +91,69 @@ trait View
     }
 
     /**
+    * Display Calendar
+    */
+    public function displayCalendar($dates, $date, $year)
+    {
+        $police = POLICE_PRIMARY;
+        ?>
+            <div class="periods">
+            <div class="year"><?php echo $year; ?></div>
+            <div class="months">
+                <ul>
+                    <?php foreach ($date->months as $id=>$m): ?>
+                        <li><a href="#" id="linkMonth<?php echo $id+1; ?>"><?php echo substr($m,0,3); ?></a></li>
+                    <?php endforeach; ?>
+                </ul>
+            </div>
+            <div class="clear"></div> 
+            <?php foreach ($dates as $m=>$days) : ?>
+            <div class="month relative" id="month<?php echo $m ?>">
+                <table>
+                    <thead>
+                        <tr>
+                        <?php foreach ($date->days as $d): ?>
+                            <th><?php echo substr($d,0,3); ?></th>
+                        <?php endforeach; ?>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <?php $end = end($days); foreach ($days as $d=>$w): ?>
+                                <?php $time = strtotime("$year-$m-$d"); ?>
+                                <?php if ($d == 1 and $w != 1): ?>
+                                    <td colspan="<?php echo $w-1; ?>" class="padding"></td>
+                                <?php endif; ?>
+                                <td<?php if($time == strtotime(date('Y-m-d'))): ?> class="today"<?php endif; ?>>
+                                    <div class="relative">
+                                        <div class="day"><?php echo $d; ?></div>
+                                    </div>
+                                    <div class="daytitle">
+                                        <?php echo $date->days[$w-1]; ?> <?php echo $d ?> <?php echo $date->months[$m-1]; ?>
+                                    </div>
+                                    <ul class="events">
+                                        <?php if(isset($events[$time])): foreach($events[$time] as $e): ?>
+                                            <li><?php echo $e; ?></li>
+                                        <?php endforeach; endif; ?>
+                                    </ul>
+                                </td>
+                                <?php if ($w == 7): ?>
+                                    </tr><tr>
+                                <?php endif; ?>
+                            <?php endforeach; ?>
+                            <?php if ($end != 7): ?>
+                                <td colspan="<?php echo 7-$end; ?>" class="padding"></td>
+                            <?php endif; ?>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        <?php endforeach; ?>
+        </div>
+        <?php
+    }
+
+    /**
     * Print warning
     */
     function alert($string, $color = PRIMARY_COLOR)
@@ -113,11 +176,5 @@ trait View
         EOT;
 
         echo $message;
-    }
-
-    //error handler function
-    function customError($errno, $errstr) {
-        echo "<b>Error:</b> [$errno] $errstr";
-        die();
     }
 }
